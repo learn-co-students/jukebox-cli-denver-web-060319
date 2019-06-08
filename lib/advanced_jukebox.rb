5d0e6e66fd072de80b6fbce04920a40d80f7e2ed
Devin Arnold <dbarnold321@gmail.com>
2019-06-08 14:30:25 -0600

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -2,16 +2,17 @@
 #make sure to edit the value of each key to replace < path to this directory >
 #with the correct path to this directory on your computer
 
-# my_songs = {
-# "Go Go GO" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/01.mp3',
-# "LiberTeens" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/02.mp3',
-# "Hamburg" =>  '< path to this directory >/jukebox-cli/audio/Emerald-Park/03.mp3',
-# "Guiding Light" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/04.mp3',
-# "Wolf" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/05.mp3',
-# "Blue" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/06.mp3',
-# "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
-# }
+my_songs = {
+"Go Go GO" => '/Users/Arnold/Desktop/Flatiron School/Labs/jukebox-cli-denver-web-060319/audio/Emerald-Park/01.mp3',
+"LiberTeens" => 'Users/Arnold/Desktop/Flatiron School/Labs/jukebox-cli-denver-web-060319/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  '/Users/Arnold/Desktop/Flatiron School/Labs/jukebox-cli-denver-web-060319/audio/Emerald-Park/03.mp3',
+"Guiding Light" => '/Users/Arnold/Desktop/Flatiron School/Labs/jukebox-cli-denver-web-060319/audio/Emerald-Park/04.mp3',
+"Wolf" => '/Users/Arnold/Desktop/Flatiron School/Labs/jukebox-cli-denver-web-060319/audio/Emerald-Park/05.mp3',
+"Blue" => '/Users/Arnold/Desktop/Flatiron School/Labs/jukebox-cli-denver-web-060319/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => 'Users/Arnold/Desktop/Flatiron School/Labs/jukebox-cli-denver-web-060319/audio/Emerald-Park/07.mp3'
+}
 
+#/Users/Arnold/Desktop/Flatiron School/Labs/jukebox-cli-denver-web-060319/audio/Emerald-Park
 def help
   #this method should be the same as in jukebox.rb
 
